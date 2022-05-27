6b1e38bd0a1fcedc55853b9caa436c9a59b532be
Ekaterina Moteva <sinkanight@mail.ru>
2022-05-27 19:26:32 +0300

    hw0 by cycles and streams

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,13 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -24,16 +26,53 @@ public class UserMealsUtil {
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
 
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
-        return null;
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        List<UserMealWithExcess> excessList = new ArrayList<>();
+
+        for (UserMeal meal : meals){
+            if (!map.containsKey(meal.getDateTime().getDayOfMonth())){
+                map.put(meal.getDateTime().getDayOfMonth(), meal.getCalories());
+            }
+            else {
+                int sum = map.get(meal.getDateTime().getDayOfMonth()) + meal.getCalories();
+                map.put(meal.getDateTime().getDayOfMonth(), sum);
+            }
+        }
+
+        for (UserMeal meal : meals){
+            if (TimeUtil.isBetweenHalfOpen(LocalTime.of(meal.getDateTime().getHour(), meal.getDateTime().getMinute()), startTime, endTime)){
+                if (map.getOrDefault(meal.getDateTime().getDayOfMonth(), caloriesPerDay)>caloriesPerDay){
+                    excessList.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), true));
+                }
+                else {
+                    excessList.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), false));
+                }
+            }
+        }
+        return excessList;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO Implement by streams
-        return null;
+        Map<Integer, Integer> map = meals.stream()
+                .collect(Collectors.groupingBy(userMeal -> userMeal.getDateTime().getDayOfMonth(), Collectors.summingInt(UserMeal::getCalories)));
+
+        List<UserMealWithExcess> trueList = meals.stream()
+                .filter(userMeal -> TimeUtil.isBetweenHalfOpen(LocalTime.of(userMeal.getDateTime().getHour(), userMeal.getDateTime().getMinute()), startTime, endTime) &&
+                        map.getOrDefault(userMeal.getDateTime().getDayOfMonth(), caloriesPerDay)>caloriesPerDay)
+                .map(userMeal -> new UserMealWithExcess(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), true))
+                .collect(Collectors.toList());
+
+        List<UserMealWithExcess> falseList =  meals.stream()
+                .filter(userMeal -> TimeUtil.isBetweenHalfOpen(LocalTime.of(userMeal.getDateTime().getHour(), userMeal.getDateTime().getMinute()), startTime, endTime) &&
+                        map.getOrDefault(userMeal.getDateTime().getDayOfMonth(), caloriesPerDay)<=caloriesPerDay)
+                .map(userMeal -> new UserMealWithExcess(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), false))
+                .collect(Collectors.toList());
+
+        return Stream.concat(trueList.stream(), falseList.stream()).collect(Collectors.toList());
     }
 }
